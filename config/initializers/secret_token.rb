3dab40e9ec56ba15c6ff0247930a2c5406cc36de
koshikawa <koshikawa@ppworks.jp>
2012-02-19 03:19:14 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tbrsample::Application.config.secret_token = 'e033218b5492d7bb0ace8d75ef6ce59cc1a639ba58985fd7c031434ebbc59388731e8916e71412461004eed7d9ef6a0f2b1899884feac019c698ef9e2b82b9db'
